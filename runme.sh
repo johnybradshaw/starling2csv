8c86d521a4fbdff655deed2eea5819f47e53d99d
John Bradshaw <noemail@nope.nope>
2018-09-17 09:13:41 +0100

    Updated to export to json

--- runme.sh
+++ runme.sh
@@ -4,8 +4,8 @@
 source .secrets
 
 ## Request
-curl "https://api.starlingbank.com/api/v1/transactions/" \
+curl 'https://api.starlingbank.com/api/v1/transactions/' \
      -H 'Cookie: __cfduid=$cookie' \
-     -H 'Authorization: Bearer $secret'
+     -H 'Authorization: Bearer $secret' > starling.json
 
 json2csv -i starling.json -p -f _embedded.transactions.created,_embedded.transactions.narrative,_embedded.transactions.source,_embedded.transactions.amount -u _embedded.transactions -B -o starling.csv
